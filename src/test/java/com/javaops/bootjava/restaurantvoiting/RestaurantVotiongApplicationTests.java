1959344f7680a2971f957843f26ac42e03324af0
EdwardTech <edwd211@gmail.com>
2021-11-27 17:26:07 +0200

    first commit

---
+++ src/test/java/com/javaops/bootjava/restaurantvoiting/RestaurantVotiongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaops.bootjava.restaurantvoiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotiongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
